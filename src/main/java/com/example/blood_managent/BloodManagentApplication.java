7b62cc0f5d4a518f2bd29fddcf7aa3ed056db743
anuj18m <octalbits18@gmail.com>
2025-09-23 14:12:05 +0530

    Initial commit - Spring Boot backend

---
+++ src/main/java/com/example/blood_managent/BloodManagentApplication.java
@@ -0,0 +1,13 @@
+package com.example.blood_managent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodManagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodManagentApplication.class, args);
+	}
+
+}
