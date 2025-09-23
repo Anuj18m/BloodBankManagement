5459126bff7b80ed6c975149a561c0af453d09df
anuj18m <octalbits18@gmail.com>
2025-09-23 14:55:44 +0530

    added sample route

---
+++ src/main/java/com/example/blood_managent/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.example.blood_managent.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Spring Boot is up and running!";
+    }
+}
